b31652c294562c98e918b1f0c489a9d0bb35889d
NarendraBabuOggu <narendrababuoggu393@gmail.com>
2020-12-16 11:21:14 +0530

    First Commit Added files to create Spark Session and read Configurtion from file.
    Also added unit tests to validate config parser and spark session objects

---
+++ tests/test_config_parser.py
@@ -0,0 +1,70 @@
+from src.dependencies.config_parser import Config
+import unittest
+from typing import Callable
+
+
+class TestConfig(unittest.TestCase):
+
+    def test_existing_file(self: Callable):
+        """
+        Method to test Config Class against existing file
+
+        Args:
+            self (Callable): Test case to validate Config Class
+        """
+
+        config = Config('tests/data/config.ini')
+        self.assertTrue(
+            config,
+            "Configuration File data/config.ini Exists but Config Class" + 
+            "Failed to Read"
+        )
+        self.assertIsInstance(
+            config,
+            Config,
+            "Configuration File should be of type Config class." + 
+            f"Where as the result is of type {type(config)}"
+        )
+
+    def test_non_existing_file(self: Callable):
+        """
+        Method to test Config Class against existing file
+
+        Args:
+            self (Callable): Test case to validate Config Class
+        """
+
+        with self.assertRaises(Exception):
+            Config('config.ini')
+
+    def test_existing_property(self: Callable):
+        """
+        Method to test Config Class against existing file
+
+        Args:
+            self (Callable): Test case to validate Config Class
+        """
+
+        config = Config('tests/data/config.ini')
+        self.assertEqual(
+            config['dev.master'],
+            'local[*]'
+        )
+
+    def test_non_existing_property(self: Callable):
+        """
+        Method to test Config Class against existing file
+
+        Args:
+            self (Callable): Test case to validate Config Class
+        """
+
+        config = Config('tests/data/config.ini')
+        self.assertNotEqual(
+            config['dev.master'],
+            'local'
+        )
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_spark.py
@@ -0,0 +1,62 @@
+from src.dependencies.utils import get_spark
+from src.dependencies.config_parser import Config
+import unittest
+from typing import Callable
+
+
+class SparkTest(unittest.TestCase):
+    def setUp(self: Callable):
+        """
+        Method to setup spark session
+
+        Args:
+            self (Callable): Test Case
+        """
+
+        self.config = Config('tests/data/config.ini')
+        self.spark, _ = get_spark(
+            loglevel='INFO', spark_config=self.config, env='dev'
+        )
+        self.spark_conf = self.spark.sparkContext.getConf()
+
+    def tearDown(self: Callable):
+        """
+        Method to Stop spark session
+
+        Args:
+            self (Callable): Test Case
+        """
+
+        self.spark.stop()
+
+    def test_appname(self: Callable):
+        """
+        Method to validate Name of Spark Application
+
+        Args:
+            self (Callable): Test Case
+        """
+
+        self.assertEqual(
+            self.spark_conf.get('spark.app.name'), 
+            'pyspark-practice',
+            "The Spark Appname should equal to the appname mentioned in Config"
+        )
+
+    def test_master(self: Callable):
+        """
+        Method to validate master of Spark Application
+
+        Args:
+            self (Callable): Test Case
+        """
+
+        self.assertEqual(
+            self.spark_conf.get('spark.master'), 
+            'local[*]',
+            "The Spark master should equal to the master mentioned in Config"
+        )
+
+
+if __name__ == '__main__':
+    unittest.main()
